684dd48d066d6d25d496a9af46c934e644f0eddb
Jordan Lesaca <jordan.m.lesaca@gmail.com>
2021-10-29 15:09:57 -0700

    added comments

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,14 +2,14 @@ class Triangle
 
   attr_accessor :x, :y, :z
 
-  def initialize(x, y, z)
+  def initialize(x, y, z) #upon initialization, accepts three arguments.
     @x = x
     @y = y
     @z = z
   end
 
   def kind
-    validate_triangle
+    validate_triangle #should raise a custom error, TriangleError
       if y == z && z == x
         :equilateral
       elsif x == y || y == z || x == z
@@ -29,7 +29,7 @@ class Triangle
 
   def validate_triangle
     raise TriangleError unless sides_greater_than_zero? && valid_triangle_inequality?
-  end
+  end #raises an error for traingles with no size, triangles with negative sides, or triangles violating trianlge inequality
 
   class TriangleError < StandardError
 
